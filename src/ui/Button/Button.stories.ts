eab184e51402867ca194f2f3e15401a83de2f81b
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-06-04 15:22:30 +0200

    feat: storybook intro

---
+++ src/ui/Button/Button.stories.ts
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import { Button } from "./Button";
+
+const meta = {
+  title: "UI/Button",
+  component: Button,
+  tags: ["autodocs"],
+  args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    label: "Button",
+  },
+};
+
+export const Red: Story = {
+  args: {
+    label: "Button",
+    bgColor: "carrot",
+    color: "clouds",
+  },
+};
+
+export const Blue: Story = {
+  args: {
+    label: "Button",
+    bgColor: "peter-river",
+    color: "carrot",
+  },
+};
---
+++ src/ui/Input/Input.stories.ts
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import { Input } from "./Input";
+
+const meta = {
+  title: "UI/Input",
+  component: Input,
+  // tags: ["autodocs"],
+  // args: { onClick: fn() },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    label: "Label",
+  },
+};
+export const Email: Story = {
+  args: {
+    label: "E-mail",
+    type: "email",
+  },
+};
+
+export const NumberField: Story = {
+  args: {
+    label: "Choose number",
+    type: "number",
+  },
+};
