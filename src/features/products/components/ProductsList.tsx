09c28379f3e659b402c7d902839ced18c23b5b15
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-06-10 15:51:23 +0200

    feat: fetch products

--- src/features/products/components/ProductsList.tsx
+++ src/features/products/components/ProductsList.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 const products = [
   {
     id: 1,
@@ -25,16 +27,55 @@ const products = [
   },
 ];
 
+// Vite
+const API_URL = import.meta.env.VITE_API_URL;
+const AIRTABLE_TOKEN = import.meta.env.VITE_AIRTABLE_TOKEN;
+
+// Webpack
+// create-react-app, prefix REACT_APP_
+// custom: plugin
+
+type ProductDto = {
+  id: number;
+  fields: {
+    name: string;
+    description: string;
+    price: number;
+  };
+};
+
 export const ProductsList = () => {
+  const [data, setData] = useState<ProductDto[]>([]);
+
+  useEffect(() => {
+    fetch(`${API_URL}/products`, {
+      headers: {
+        Authorization: `Bearer ${AIRTABLE_TOKEN}`,
+        "content-type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Response error");
+      })
+      .then((data) => {
+        // console.log({ data });
+        // setData
+        setData(data.records);
+      });
+  }, []);
+
   return (
     <div>
       <h2>Products</h2>
 
-      {products.map((elem) => (
+      {data.map((elem) => (
         <div key={elem.id}>
           <h4>{elem.fields.name}</h4>
           <p>
-            {elem.fields.description}, {elem.fields.price}
+            {elem.fields.description}, ${elem.fields.price}
           </p>
           <br />
         </div>
