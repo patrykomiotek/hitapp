488d26d5a7358a6dc0ddae7b052837feb7776358
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-06-11 15:01:46 +0200

    feat: Stepper component

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -1,5 +1,5 @@
-import { useReducer } from "react";
-import { Button } from "../../ui";
+import { KeyboardEventHandler, useReducer, useRef } from "react";
+import { Button, Input } from "../../ui";
 
 type State = {
   count: number;
@@ -8,11 +8,13 @@ type State = {
 enum ActionType {
   INCREMENT = "increment",
   DECREMENT = "decrement",
+  SET_VALUE = "set-value",
 }
 
 // provide descriminated union
 type Action = {
   type: ActionType;
+  payload?: number;
 };
 
 const initialState: State = { count: 0 };
@@ -23,6 +25,11 @@ const reducer = (state: State, action: Action) => {
       return { count: state.count - 1 };
     case ActionType.INCREMENT:
       return { count: state.count + 1 };
+    case ActionType.SET_VALUE:
+      if (action.payload) {
+        return { count: action.payload };
+      }
+      return state;
     default:
       return state;
   }
@@ -30,15 +37,32 @@ const reducer = (state: State, action: Action) => {
 
 const increase = () => ({ type: ActionType.INCREMENT });
 const decrease = () => ({ type: ActionType.DECREMENT });
+const setValue = (value: number) => ({
+  type: ActionType.SET_VALUE,
+  payload: value,
+});
 
 export const Stepper = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const valueFieldRef = useRef<HTMLInputElement>(null);
+
+  const handleOnKeyEvent: KeyboardEventHandler<HTMLInputElement> = (event) => {
+    // console.log({ event: event.key });
+    if (valueFieldRef.current) {
+      const value = parseInt(valueFieldRef.current.value, 10) || 0;
+
+      if (event.key === "Enter") {
+        dispatch(setValue(value));
+      }
+    }
+  };
 
   return (
     <div>
       <Button label="Decrease" onClick={() => dispatch(decrease())} />
       <span>{state.count}</span>
       <Button label="Increase" onClick={() => dispatch(increase())} />
+      <Input label="Value" ref={valueFieldRef} onKeyDown={handleOnKeyEvent} />
     </div>
   );
 };
