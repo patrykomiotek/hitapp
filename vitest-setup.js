32ce316522a71115a2673a9c27765d1fc3455f2b
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-06-12 09:13:14 +0200

    fix: tests

--- vitest-setup.js
+++ vitest-setup.js
@@ -1,1 +1,12 @@
+// import { afterEach } from 'vitest'
+// import { cleanup } from '@testing-library/react'
 import "@testing-library/jest-dom/vitest";
+import "@testing-library/jest-dom";
+
+import { toHaveNoViolations } from "jest-axe";
+
+expect.extend(toHaveNoViolations);
+
+// afterEach(() => {
+//   cleanup();
+// })
