6cd707ea9452b09def6be3c6d6c9368e91386655
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-06-12 13:21:55 +0200

    feat: ViewPort component part two

--- src/components/ViewPort/ViewPort.spec.tsx
+++ src/components/ViewPort/ViewPort.spec.tsx
@@ -1,17 +1,41 @@
 import { vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { ViewPort } from "./ViewPort";
+import { useViewPort } from "../../hooks/useViewPort";
 
-// jest.mock('../../hooks/useViewPort', () => {
+// jest.mock("../../hooks/useViewPort", () => ({
+//   useViewPort: jest.fn(), // useViewPort -> undefined
+// }));
+
 vi.mock("../../hooks/useViewPort", () => ({
-  useViewPort: vi.fn().mockImplementation(() => ({ x: 100, y: 200 })),
+  // useViewPort: vi.fn().mockImplementation(() => ({ x: 100, y: 200 })),
+  useViewPort: vi.fn(), // useViewPort -> undefined
 }));
+
+// jest only
+const mockedUseViewPort = useViewPort as jest.Mock;
+// const mockedUseViewPort = useViewPort as
 
 describe("ViewPort component", () => {
   it("should display values", () => {
+    // for the jest
+    mockedUseViewPort.mockImplementationOnce(() => ({ x: 100, y: 200 }));
+    // for the vitest
+    // const mock = vi.fn().mockImplementation(useViewPort);
+    // mock.mockImplementationOnce(() => ({ x: 100, y: 200 }));
+
     const { debug } = render(<ViewPort />);
     debug();
-    // expect(screen.getByText(/x: 1024, y: 768/i)).toBeInTheDocument();
     expect(screen.getByText(/x: 100, y: 200/i)).toBeInTheDocument();
+  });
+  it("should display different values", () => {
+    // for the jest
+    mockedUseViewPort.mockImplementationOnce(() => ({ x: 300, y: 200 }));
+    // const mock = vi.fn().mockImplementation(useViewPort);
+    // mock.mockImplementationOnce(() => ({ x: 300, y: 200 }));
+    const { debug } = render(<ViewPort />);
+    expect(mockedUseViewPort).toHaveBeenCalledTimes(2);
+    debug();
+    expect(screen.getByText(/x: 300, y: 200/i)).toBeInTheDocument();
   });
 });
